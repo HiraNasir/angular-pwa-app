d39f841a15e5822bec9cb8f0f37711df865be43a
HiraNasir <hiraanasir16@gmail.com>
2019-10-30 13:02:39 +0500

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,22 @@ import { SwPush } from '@angular/service-worker';
 export class AppComponent {
 
   update: boolean = false;
-  title = 'angular-PWA';
-
+  
+   VAPID_PUBLIC = "BNCXKhTTGNah_ZjSesYKw08qbM2GfAHB_sJUw2PLs0X4QhkMRA-ngrfqgPGPa70OluaXNCLLM7QeTorT2wkqebw";
   constructor(update: SwUpdate, swPush: SwPush) {
     update.available.subscribe(event => {
       this.update = true;
       console.log(this.update)
     })
     if (swPush.isEnabled) {
-      console.log ('pushed')
+      swPush
+        .requestSubscription({
+          serverPublicKey: this.VAPID_PUBLIC,
+        })
+        .then(subscription => {
+          // send subscription to the server
+        })
+        .catch(console.error)
     }
 
   }
