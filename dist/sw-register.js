2a7d66065ed0fc66911ae37c49a353157236ad6b
HiraNasir <hiraanasir16@gmail.com>
2019-10-30 15:16:09 +0500

    push

--- dist/sw-register.js
+++
@@ -1,8 +0,0 @@
-// if('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('./sw.js', {
-//         scope: '/'
-//     })
-//     .then(registeration => {
-//         console.log('registered successfully');
-//     })
-// };
--- dist/sw.js
+++
@@ -1,6 +0,0 @@
-// const VERSION = 'v1';
-
-// function log (messages){
-//     console.log(VERSION, messages)
-// }
-// log('installing service worker');
