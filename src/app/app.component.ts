36a9fa475c18820e7965cedfa49a8c439e65c104
HiraNasir <hiraanasir16@gmail.com>
2019-10-30 16:42:10 +0500

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
 
   update: boolean = false;
   defferedPrompt: any;
-   addBtn = document.querySelector('.add-button');
+  addBtn = document.querySelector('.add-button');
   VAPID_PUBLIC = "BNCXKhTTGNah_ZjSesYKw08qbM2GfAHB_sJUw2PLs0X4QhkMRA-ngrfqgPGPa70OluaXNCLLM7QeTorT2wkqebw";
   constructor(update: SwUpdate, swPush: SwPush, pushService: PushNotificationService) {
 
@@ -37,27 +37,24 @@ export class AppComponent {
       e.preventDefault();
       // Stash the event so it can be triggered later.
       this.deferredPrompt = e;
-      var a2hsBtn = document.querySelector(".ad2hs-prompt");
-    
-      a2hsBtn.addEventListener("click", addToHomeScreen);
-     function  addToHomeScreen() {  var a2hsBtn = document.querySelector(".ad2hs-prompt");  // hide our user interface that shows our A2HS button
-      // Show the prompt
-      this.defferedPrompt.prompt();  // Wait for the user to respond to the prompt
-      this.defferedPrompt.userChoice
-        .then(function(choiceResult){
-    
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-    
-      this.defferedPrompt = null;
-    
-    });}
-    
-    });
+      return false;
 
+      function addToHomeScreen() {
+        this.defferedPrompt.prompt();  // Wait for the user to respond to the prompt
+        this.defferedPrompt.userChoice
+          .then(function (choiceResult) {
+
+            if (choiceResult.outcome === 'accepted') {
+              console.log('User accepted the A2HS prompt');
+            } else {
+              console.log('User dismissed the A2HS prompt');
+            }
+
+            this.defferedPrompt = null;
+
+          });
+      }
+    });
 
     // window.addEventListener('beforeinstallprompt', (e) => {
     //   // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -65,7 +62,7 @@ export class AppComponent {
     //   // Stash the event so it can be triggered later.
     //   this.defferedPrompt = e;
     //   // Update UI to notify the user they can add to home screen
-      
+
 
     //   this.addBtn.addEventListener('click', (e) => {
     //     // hide our user interface that shows our A2HS button
